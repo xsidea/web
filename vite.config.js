7749abdc2815c05b2a34d31250081cbcf2c82d37
xiaosong <lianghaisong@qq.com>
2024-10-23 01:42:12 +0800

    初始化

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const env = loadEnv('', process.cwd())
 
 // https://vitejs.dev/config/
 export default defineConfig({
